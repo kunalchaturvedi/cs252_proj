2b31ff5e49b9ce164f692d73be98627ef01a6790
shashaankkaim <sshank@iitk.ac.in>
2016-11-10 16:26:50 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import imaplib
+import email
+
+def extract_body(payload):
+    if isinstance(payload,str):
+        return payload
+    else:
+        return '\n'.join([extract_body(part.get_payload()) for part in payload])
+temp=open('datamail.txt','w+')
+conn = imaplib.IMAP4_SSL("imap.gmail.com", '993')
+conn.login("lawdjesusrocks", "qmesf40241")
+conn.select()
+typ, data = conn.search(None, 'UNSEEN')
+try:
+    for num in data[0].split():
+        typ, msg_data = conn.fetch(num, '(RFC822)')
+        for response_part in msg_data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_string(response_part[1])
+                subject=msg['subject']                   
+                print(subject)
+                payload=msg.get_payload()
+                body=extract_body(payload)
+                print(body,file = temp)
+        typ, response = conn.store(num, '+FLAGS', r'(\Seen)')
+finally:
+    try:
+        conn.close()
+    except:
+        pass
+    conn.logout()
