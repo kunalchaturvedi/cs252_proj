b0577319bcad015111b827a54c7ca2c9d8dbe38f
shashaankkaim <sshank@iitk.ac.in>
2016-11-10 18:50:48 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
 <?
 session_start();
 session_destroy();
-
+if(file_exists("thefinal.csv")){
+unlink("thefinal.csv");}
+if(file_exists("datamail.txt")){
+unlink("datamail.txt");}
+if(file_exists("datamail2.txt")){
+unlink("datamail2.txt");}
 header("Location: myindex.php");
 ?>
